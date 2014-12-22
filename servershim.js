fcd3e9ad1afd76493ec44a7d710513fe1e8fff9e
Ryan Day <soldair@gmail.com>
2014-12-22 07:42:44 -0800

    fixing js error when running highcharts in node
    from 4.0.4 pull. Date added to servershim as now window.Date is required

--- servershim.js
+++ servershim.js
@@ -3,4 +3,4 @@
 
 var document = {createElement:function(name){ return {getContext:function(){}} },documentElement:{}};
 var navigator = {userAgent:'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.3 Safari/537.36'};
-var window = {navigator:navigator};
+var window = {navigator:navigator,Date:Date};
