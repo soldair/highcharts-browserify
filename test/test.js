7a8562531fdb6b0358f5f82cbbae5ad8d5b1b80a
achingbrain <alex@achingbrain.net>
2015-10-20 10:57:08 +0100

    Removes bundled highcharts distribution
    Instead adds highcharts-release as a dependency and uses files from that instead.
    
    Also uses the bundled standalone-framework instead of jquery as it's small, self-contained and doesn't run the risk of conflicting with other libaries on the page.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 var test = require('tape');
 
+global.document = {createElement:function(name){ return {getContext:function(){}} },documentElement:{}};
+global.navigator = {userAgent:'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.3 Safari/537.36'};
+global.window = {navigator:navigator,Date:Date};
 
 test("can require hc",function(t){
-
-  var hc = require('../index');
+  var hc = require('../');
 
   t.ok(hc,'should have export');
   t.end();
